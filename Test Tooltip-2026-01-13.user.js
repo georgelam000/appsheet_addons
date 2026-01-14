384cc7a1b6c3e352459e652fa69b5aff1327b72a
lucyma-gs <lucy_ma@carrefour.com>
2026-01-14 16:24:43 +0800

    Add files via upload

---
+++ Test Tooltip-2026-01-13.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Test Tooltip
+// @namespace    https://www.appsheet.com/
+// @version      2026-01-13
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.appsheet.com/*40facf3a-3b61-407a-b93a-468bedc2891f*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.7.1.js
+// @require      https://code.jquery.com/ui/1.14.1/jquery-ui.js
+// ==/UserScript==
+/* global $ */
+(function() {
+    'use strict';
+
+    // Your code here...
+    $(document).ready(function() {
+        var i = 1;
+        while ( i < 60) {
+            if ($("div[role='heading']:first") != undefined) {
+                setTimeout(function(){
+                    var tipcontent = "&nbsp;<a href='#' title='This is tooltip'><i class='far fa-question-circle'></i></a>";
+                    $("div[role='heading']:first").append(tipcontent);
+                },1000);
+                break;
+            }
+            i++;
+        }
+    });
+})();
