1111ad0599dc0ef747514bfc36713651fe996db1
lucyma-gs <lucy_ma@carrefour.com>
2026-01-21 17:47:03 +0800

    Update Test Tooltip-2026-01-13.user.js
    call api by cell

--- Test Tooltip-2026-01-13.user.js
+++ Test Tooltip-2026-01-13.user.js
@@ -25,6 +25,47 @@
                var initHtml = $(this).html();
                $( this ).html("<a href='#' title='" + initText + "'>" + initHtml + "</a>");
            });
+           $("div[data-testonly-column='Version_Number']").each(function( index ) {
+               var initHtml = $(this).html();
+               var rowElement = $(this).parent().parent();
+               var PSS_Supplier_Code = rowElement.find("div[data-testonly-column='PSS_Supplier_Code'").text
+               var Product_barcode = rowElement.find("div[data-testonly-column='Product_barcode'").text();
+               var Master_Barcode = rowElement.find("div[data-testonly-column='Master_Barcode'").text();
+               var PSS_Client_Code = rowElement.find("div[data-testonly-column='PSS_Client_Code'").text();
+               var PSS_Warehouse = rowElement.find("div[data-testonly-column='PSS_Warehouse'").text();
+               var selectorStr = "TOP(Orderby(Filter(supply_plan_full_by_month, AND([PSS_Supplier_Code]='" + PSS_Supplier_Code
+               + "', [Product_barcode]='" + Product_barcode
+               + "', [Master_Barcode]='" + Master_Barcode
+               + "', [PSS_Client_Code]='" + PSS_Client_Code
+               + "', [PSS_Warehouse]='" + PSS_Warehouse+"')), [Updated_Date], false), 1)";
+               GM.xmlHttpRequest({
+                method: "POST",
+                url: "/api/v2/apps/40facf3a-3b61-407a-b93a-468bedc2891f/tables/supply_plan_full_by_month/Action",
+                headers: {
+                    "Content-Type": "application/json",
+                    "applicationAccessKey": "V2-iFDBP-2pjIr-3xY3p-sqTrj-CC4FR-q2r5S-RwYZJ-xBTMX"
+                },
+                data: JSON.stringify({
+                    "Action": "Find",
+                     "Properties": {
+                         "Locale": "en-US",
+                         "Location": "47.623098, -122.330184",
+                         "Timezone": "Pacific Standard Time",
+                         "Selector": selectorStr
+                     },
+                    "Rows": []
+                }),
+                onload: function(response) {
+                    console.log(response.responseText );
+                    var resultData = JSON.parse(response.responseText);
+                    var msg = "no previous version data";
+                    if (resultData.length > 0) {
+                        msg = resultData[0].Updated_Date + ": " + resultData[0].Product_barcode;
+                    }
+                    $( this ).html("<a href='#' title='" + msg + "'>" + initHtml + "</a>");
+                }
+            }).catch(e => console.error(e));
+           });
             GM.xmlHttpRequest({
                 method: "POST",
                 url: "/api/v2/apps/40facf3a-3b61-407a-b93a-468bedc2891f/tables/supply_plan_param/Action",
@@ -38,7 +79,7 @@
                          "Locale": "en-US",
                          "Location": "47.623098, -122.330184",
                          "Timezone": "Pacific Standard Time",
-                         "Selector": "Filter(supply_plan_param, AND([PSS_Supplier_Code] ='MUSTN', [PSS_Client_Code]='FRCA'))"
+                         "Selector": "TOP(Orderby(Filter(supply_plan_param, AND([PSS_Supplier_Code] ='MUSTN', [PSS_Client_Code]='FRCA')), [POD], false), 1)"
                      },
                     "Rows": []
                 }),
